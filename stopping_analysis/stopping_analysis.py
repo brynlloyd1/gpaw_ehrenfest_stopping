1f7ea883bebc3f1d39a8a512433ff1867b440616
bryn lloyd <bryn.lloyd@jesus.ox.ac.uk>
2025-05-22 15:35:10 +0100

    added sliding window fitting to extract stopping powers from kinetic energy data

--- stopping_analysis/stopping_analysis.py
+++ stopping_analysis/stopping_analysis.py
@@ -19,6 +19,9 @@ class StoppingPowerAnalysis:
         self.data_directory = data_directory
         self.filenames = self.extract_gpaw_files()
         print(json.dumps(self.filenames, indent=4))
+
+        # TODO: if theres multiple energies, and you write for each of them
+        # TODO: atm it just overwrites the same file multiple times
         self.trajectory_file = "output.xyz"
 
         self.atoms_dict = {}
@@ -30,9 +33,8 @@ class StoppingPowerAnalysis:
             energy_to_write = "40 keV"
             self.write_data(energy_to_write)
 
-        self.kinetic_energies = {energy: [atoms.get_kinetic_energy()
-                                          for atoms in atoms_list]
-                                 for energy, atoms_list in self.atoms_dict.items()}
+        self.kinetic_energies = self.get_projectile_kinetic_energies()
+        self.projectile_positions = self.get_projectile_positions()
 
     def extract_gpaw_files(self):
         """
@@ -102,25 +104,92 @@ class StoppingPowerAnalysis:
         for i, atoms in enumerate(self.atoms_dict[energy]):
             write(self.trajectory_file, atoms, append=i > 0)
 
+    def get_projectile_kinetic_energies(self):
+        kinetic_energies = {energy: [atoms.get_kinetic_energy()
+                                     for atoms in atoms_list]
+                            for energy, atoms_list in self.atoms_dict.items()}
+        return kinetic_energies
+
+    def get_projectile_positions(self):
+        projectile_positions = {energy: [atoms.get_positions()[-1][0]
+                                         for atoms in atoms_list]
+                                for energy, atoms_list in self.atoms_dict.items()}
+        return projectile_positions
+
     def plot_kinetic_energies(self):
-        """
-        extract kinetic energy from the atoms instances stored inside self.atoms_dict[energy]
-        then plot, with a subplots for each energy
-        """
-
         n_subplots = len(self.kinetic_energies.keys())
-        fig, axs = plt.subplots(n_subplots, figsize=(10, 5*n_subplots))
+        fig, axs = plt.subplots(n_subplots, figsize=(
+            10, 5*n_subplots), sharex=True)
+        # so that is works for the =1 case
+        if n_subplots == 1:
+            axs = [axs]
 
-        for i, (energy, kinetic_energies) in enumerate(self.kinetic_energies.items()):
-            axs[i].plot(kinetic_energies)
+        fig.suptitle("Fitting KE to extract stopping powers")
 
+        axs[-1].set_xlabel(r"projectile position [$\AA$]")
+        _ = [ax.set_ylabel("kinetic energy [keV]") for ax in axs]
+
+        for i in range(n_subplots):
+            energy, kinetic_energies = list(self.kinetic_energies.items())[i]
+            kinetic_energies = np.array(
+                kinetic_energies) * 1e-3  # convert from eV to keV
+            _, projectile_positions = list(
+                self.projectile_positions.items())[i]
+
+            ##################################
+            # PERFORM SLIDING WINDOW FITTING #
+            #     and get stopping power
+            ##################################
+
+            # TODO: should be able to calculat the minimum window size from the size of the unit cell and from the distance travelled per timestep
+            fit, cov, x_window, y_window = utils.sliding_fit(
+                projectile_positions, kinetic_energies, 3, 5)
+            stopping_power = -fit[0]
+            stopping_power_uncertainty = np.sqrt(cov[0][0])
+
+            axs[i].set_title(f"Projectile Initial energy: {energy}")
+            axs[i].plot(projectile_positions, kinetic_energies, "x")
+            axs[i].plot(x_window, y_window, "x", color="red")
+            axs[i].plot(x_window, np.poly1d(fit)(x_window), color="red",
+                        label=rf"$S_e$ = {1e3*stopping_power:.1f} $\pm$ {1e3*stopping_power_uncertainty:.1f} [eV/$\AA$]")
+
+        print(projectile_positions)
+        print(kinetic_energies)
+
+        _ = [ax.legend() for ax in axs]
         plt.show()
+
+    # def plot_kinetic_energies(self):
+    #     """
+    #     extract kinetic energy from the atoms instances stored inside self.atoms_dict[energy]
+    #     then plot, with a subplots for each energy
+    #     """
+    #
+    #     n_subplots = len(self.kinetic_energies.keys())
+    #     fig, axs = plt.subplots(n_subplots, figsize=(10, 5*n_subplots))
+    #     # TODO: add title and axis labels
+    #     # TODO: add fitting to extract stopping powers - might be hard to account for different sizes of supercells
+    #
+    #     # so that is works for the =1 case
+    #     if n_subplots == 1:
+    #         axs = [axs]
+    #
+    #     for i in range(n_subplots):
+    #         energy, kinetic_energies = list(self.kinetic_energies.items())[i]
+    #         _, projectile_positions = list(
+    #             self.projectile_positions.items())[i]
+    #
+    #         print(projectile_positions, kinetic_energies)
+    #
+    #         axs[i].plot(projectile_positions, kinetic_energies)
+    #
+    #     plt.show()
 
 
 if __name__ == "__main__":
 
-    data_directory = "/Users/brynlloyd/Developer/Coding/Python/dft/gpaw/data/small_unitcell/"
-    # data_directory = "/Users/brynlloyd/Developer/Coding/Python/dft/gpaw/data/larger_unitcell/"
+    # data_directory = "/Users/brynlloyd/Developer/Coding/Python/dft/gpaw/my_own_stopping/data/small_unitcell/"
+    data_directory = "/Users/brynlloyd/Developer/Coding/Python/dft/gpaw/my_own_stopping/data/larger_unitcell/"
 
     analysis = StoppingPowerAnalysis(data_directory)
     analysis.plot_kinetic_energies()
--- stopping_analysis/utils.py
+++ stopping_analysis/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def append_to_dict(dictionary, key, value):
     """
     appends to a dictionary, where the value is a list of elements
@@ -14,3 +17,43 @@ def append_to_dict(dictionary, key, value):
         dictionary[key].append(value)
 
     return dictionary
+
+
+def calculate_r2(x, y, y_fit):
+    ss_res = np.sum((y - y_fit)**2)
+    ss_tot = np.sum((y - np.mean(y))**2)
+    r2 = 1 - ss_res / ss_tot if ss_tot != 0 else -np.inf
+    return r2
+
+
+def sliding_fit(x, y, min_window_size, max_window_size=None):
+    if max_window_size is None:
+        max_window_size = len(x)
+
+    # initialize variables for tracking the best fit
+    best_r2 = -np.inf
+    best_fit = None
+    best_cov = None
+    best_x = None
+    best_y = None
+
+    # iterates over window size and starting position to find the optimum fit
+    for window_size in range(min_window_size, len(x)):
+        for i in range(len(x) - window_size):
+            x_window = x[i: i+window_size]
+            y_window = y[i: i+window_size]
+
+            fit, cov = np.polyfit(x_window, y_window, 1, cov=True)
+            pfit = np.poly1d(fit)
+            y_fit = pfit(x_window)
+
+            r2 = calculate_r2(x_window, y_window, y_fit)
+
+            if r2 > best_r2:
+                best_r2 = r2
+                best_fit = fit
+                best_cov = cov
+                best_x = x_window
+                best_y = y_window
+
+    return best_fit, best_cov, best_x, best_y
