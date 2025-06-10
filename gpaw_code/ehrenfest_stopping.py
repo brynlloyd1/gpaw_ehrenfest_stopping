2d55c091d975006ab47faaabc1449338b257b536
bryn lloyd <bryn.lloyd@jesus.ox.ac.uk>
2025-06-10 13:40:40 +0100

    update gpaw code

--- gpaw_code/ehrenfest_stopping.py
+++ gpaw_code/ehrenfest_stopping.py
@@ -1,8 +1,6 @@
 from gpaw.tddft import TDDFT
 from gpaw.tddft.ehrenfest import EhrenfestVelocityVerlet
-
 from ase.units import Bohr, AUT, _amu, _me, Hartree
-
 import numpy as np
 
 ####################
@@ -10,7 +8,7 @@ import numpy as np
 ####################
 
 name = "Al_stopping"
-kinetic_energy = 40e3
+kinetic_energy = 10e3
 filename_body = f"{name}_{str(round(kinetic_energy*1e-3))}k"
 
 
@@ -20,11 +18,9 @@ filename_body = f"{name}_{str(round(kinetic_energy*1e-3))}k"
 
 
 # will need to add parallel argument to get parallelisation to work
-td_calc = TDDFT(filename=name + ".gpw",                # file containing the ground state from which to propagate
-                # time propagator for the Kohn-Sham equations - need this one for ehrenfest
-                propagator="EFSICN",
-                solver="BiCGStab",                     # solver for the propagator
-                txt="output.txt")
+td_calc = TDDFT(filename=name + "_ground_state" + ".gpw",                # file containing the ground state
+                propagator="EFSICN",                                     # time propagator for the Kohn-Sham equations - need this one for ehrenfest
+                solver="BiCGStab")                                       # solver for the propagator
 
 
 #########################
@@ -35,8 +31,19 @@ projectile_index = len(td_calc.atoms.positions) - 1
 initial_velocities = np.zeros_like(td_calc.atoms.get_velocities())
 projectile_mass = td_calc.atoms.get_masses()[projectile_index] * (_amu / _me)
 kinetic_energy *= 1 / Hartree
-initial_velocities[projectile_index, 0] = np.sqrt(
-    (2 * kinetic_energy) / projectile_mass) * Bohr / AUT
+
+# UNCOMMENT FOR HYPERCHANNELLING TRAJECTORY
+#initial_velocities[projectile_index, 0] = np.sqrt((2 * kinetic_energy) / projectile_mass) * Bohr / AUT
+
+# UNCOMMENT FOR PRESAMPLED TRAJECTORY
+speed = np.sqrt((2 * kinetic_energy) / projectile_mass) * Bohr / AUT
+initial_direction = np.array([-0.39661179, -0.60274438, 0.69238594])
+initial_velocities[projectile_index] = (speed * np.array(initial_direction)).tolist()
+
+# check that the projectile kinetic energy is correct
+projectile_KE_check = (np.linalg.norm(initial_velocities[projectile_index]) * AUT / Bohr)**2 * projectile_mass/2
+print(f"""calculated projectile KE: {projectile_KE_check}
+       expected: {kinetic_energy} """)
 
 td_calc.atoms.set_velocities(initial_velocities)
 
@@ -45,21 +52,15 @@ td_calc.atoms.set_velocities(initial_velocities)
 # run ehrenfest dynamics #
 ##########################
 
-# this is what is used by the example
-timestep = np.sqrt(1e3 / kinetic_energy) * 16.0
-
+timestep = np.sqrt(1e3 / kinetic_energy) * 16.0         # this is what is used by the example
 
 niters = 100
-# should play around with this
-# saving every 10 does not give enough data
-# could potentially save every timestep within a given range, and otherwise save less regularly?
-save_every = 2
+save_every = 1
 
 evv = EhrenfestVelocityVerlet(td_calc)
 for i in range(niters):
 
     print(f"timestep: {i+1} / {niters}")
-
     if i != 0 and i % save_every == 0:
         td_calc.write(filename_body + "_step" + str(i) + ".gpw")
 
--- gpaw_code/find_ground_state.py
+++ gpaw_code/find_ground_state.py
@@ -1,15 +1,11 @@
-from gpaw import GPAW, RMMDIIS
-from gpaw.external import ConstantPotential
-from gpaw.utilities import h2gpts
+from gpaw import GPAW, FermiDirac
 
 from ase.lattice.cubic import FaceCenteredCubic
 from ase import Atoms
-from ase.units import Bohr
-
-import numpy as np
 
 
-name = "Al_stopping"
+
+name = "Al_stopping_ground_state"
 
 ##########################
 # create al + projectile #
@@ -17,88 +13,33 @@ name = "Al_stopping"
 
 AL_LATTICE_CONSTANT = 4.05
 al_supercell = FaceCenteredCubic("Al",
-                                 size=(1, 2, 2),
+                                 size=(3, 2, 2),
                                  latticeconstant=AL_LATTICE_CONSTANT)
-al_supercell.center(vacuum=15.0, axis=0)
+# al_supercell.center(vacuum=15.0, axis=0)
 al_supercell.center()
 
-projectile_starting_position = [5.0, AL_LATTICE_CONSTANT, AL_LATTICE_CONSTANT]
+# HYPERCHANNELLING STARTING POSITION
+# projectile_starting_position = [0.0, AL_LATTICE_CONSTANT, AL_LATTICE_CONSTANT]
+
+# PRESAMPLED TRAJECTORY STARTING POSITION
+projectile_starting_position = [3.83534014, 2.7002751, 0.44542105]
 projectile = Atoms("H",
                    cell=al_supercell.cell,
                    positions=[projectile_starting_position])
 
-system = al_supercell  # + projectile
-# PREVIOUSLY HAD PBC'S ON (IN ALL THREE DIRECTIONS?? WHICH DOESNT MAKE SENSE BC I ADD VACUUM TO THE END...)
-# system.set_pbc(True)
-
+system = al_supercell  + projectile
 
 #####################
 # find ground state #
 #####################
 
-# supposedly two-stage convergence is required for stopping of a charged particle
-conv_fast = {"energy": 1.0, "density": 1.0, "eigenstates": 1.0}
-conv_par = {"energy": 1e-3, "density": 1e-3, "eigenstates": 1e-7}
-
-
-constant_potential = ConstantPotential(1.0)
-
-
-# first ground_state calculation with fast convergence
-calc = GPAW(mode='fd',
-            gpts=h2gpts(0.2, al_supercell.cell, idiv=8),
-            nbands=110,
-            xc='LDA',
-            charge=1,
-            txt=f"{name}_ground_state.txt",
-            convergence=conv_fast,
-            external=constant_potential,
-            symmetry={'point_group': False})
+calc = GPAW(mode = "fd",
+            occupations = FermiDirac(0.02),
+            symmetry = {"point_group": False},
+            txt = "output.txt")
 
 
 system.calc = calc
 system.get_potential_energy()
-
-print("""
-
-FIRST CALCULATION FINISHED!!
-
-""")
-
-
-# second ground state calculation with par?? convergence
-
-# External potential used to prevent charge tranfer from graphene to ion.
-A = 1.0
-X0 = system.positions[-1] / Bohr
-rcut = 3.0 / Bohr
-vext = calc.hamiltonian.vext
-gd = calc.hamiltonian.finegd
-n_c = gd.n_c
-h_c = gd.get_grid_spacings()
-b_c = gd.beg_c
-vext.vext_g.flags.writeable = True
-vext.vext_g[:] = 0.0
-for i in range(n_c[0]):
-    for j in range(n_c[1]):
-        for k in range(n_c[2]):
-            x = h_c[0] * (b_c[0] + i)
-            y = h_c[1] * (b_c[1] + j)
-            z = h_c[2] * (b_c[2] + k)
-            X = np.array([x, y, z])
-            dist = np.linalg.norm(X - X0)
-            if dist < rcut:
-                vext.vext_g[i, j, k] += A * np.exp(-dist**2)
-
-system.calc = calc.new(convergence=conv_par, eigensolver=RMMDIIS(
-    5), external=vext, txt=f"{name}_vext_gs.txt")
-
-system.get_potential_energy()
-
-print("""
-
-FIRST CALCULATION FINISHED!!
-
-""")
-
+system.calc.write(name + '.gpw', mode='all')
 system.calc.write(name + '.gpw', mode='all')
